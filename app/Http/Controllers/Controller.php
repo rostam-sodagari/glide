b852466657290461e56b1d11f47d9a814ae162d2
Amir Sodagari <a.sodagari@adam-barfi.ir>
2026-02-06 16:45:31 +0000

    Remove success and error response methods from base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,31 +5,5 @@ namespace App\Http\Controllers;
 // This is a base controller class that other controllers can extend.
 abstract class Controller
 {
-    /**
-     * Return a successful response with the provided data.
-     *
-     * @param mixed $data The data to be included in the success response.
-     * @return \Illuminate\Http\Response The HTTP response object.
-     */
-             public function success($data): \Illuminate\Http\Response{
-        return response()->json([
-            'status' => 'success',
-            'data' => $data
-        ]);
-    }
-
-    /**
-     * Return an error response.
-     *
-     * @param string $message The error message to return
-     * @param int $code The HTTP status code (default: 400)
-     * @return \Illuminate\Http\Response
-     */
-    public function error($message, $code = 400): \Illuminate\Http\Response{
-        
-        return response()->json([
-            'status' => 'error',
-            'message' => $message
-        ], $code);
-    }
+  
 }
